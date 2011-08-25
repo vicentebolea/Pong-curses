71be1cc576fc16582142503b4e5898fa4c8f7900
Vicente Adolfo Bolea Sánchez <vicente.bolea@gmail.com>
2011-08-25 23:48:54 +0900

    tan bonito

--- pong.c
+++ pong.c
@@ -1,14 +1,11 @@
 #include <ncurses.h>		
 #include <stdlib.h>
 #include <stdbool.h>
-struct objeto{ short int x; short int y; short int c;};
+typedef struct objeto{ short int x; short int y; short int c; bool movver; bool movhor;};
 bool cbool(bool);
-bool cbool(bool a) {return a=(a==true) ? false : true;} 
+bool cbool(bool) {return a=(a==true) ? false : true;}
 
-void main() {
-	struct objeto b1,b2,b,s;
-	int control; int cont=0;
-	bool fin = false; bool movhor=true; bool movver=true;
+void initgame(objeto *b1,objeto *b2, objeto *scr){
 	
 	initscr();					//inicia la pantalla
 	start_color();	
@@ -17,9 +14,9 @@ void main() {
 	curs_set(0);					
 	nodelay(stdscr,0); 
 	getmaxyx(stdscr,s.y,s.x);
-	b1.x=s.x-2; b2.x=1;
-	b1.y=s.y/2; b2.y=s.y/2;
-	b1.c=0;		b2.c=0;
+	b1->x=scr->x-2; b2->x=1;
+	b1->y=scr->y/2; b2->y=scr->y/2;
+	b1->c=0;	b2->c=0;
 	mvprintw(4,5,"          oooooooooo                                   "); 
         mvprintw(5,5,"          888    888  ooooooo  oo oooooo     oooooooo8 ");
         mvprintw(6,5,"          888oooo88 888     888 888   888  888    88o  ");
@@ -30,44 +27,44 @@ void main() {
       	getch();
 	erase();
 	
-	mvprintw(s.y/2,s.x/3,"Jugador1 tus controles son 'a' y 'q'");
-	mvprintw(1+s.y/2,s.x/3,"Jugador2 tus controles son las flechas del teclado");
-	mvprintw(2+s.y/2,s.x/3,"Pulse ENTER para empezar");
+	mvprintw(scr->y/2,scr->x/3,"Jugador1 tus controles son 'a' y 'q'");
+	mvprintw(1+scr->y/2,scr->x/3,"Jugador2 tus controles son las flechas del teclado");
+	mvprintw(2+scr->y/2,scr->x/3,"Pulse ENTER para empezar");
 	getch();
 			
 	nodelay(stdscr,1);		
 	init_pair(1,COLOR_RED,COLOR_BLACK);
 	init_pair(2,COLOR_BLUE,COLOR_BLACK);
-	init_pair(3,COLOR_WHITE,COLOR_BLACK);
-	goto inicio;
+	init_pair(3,COLOR_WHITE,COLOR_BLACK); 
+}
+void engine(objeto *a,objeto *b1,objeto *b2,objeto *scr) {
 
+	a->movver=((a->y==scr->y-1)||(a->y==1)) ? cbool(a->movver);
+	if ((b->x>=src->x-2)||(b->x<=2)){
+		a->movhor=cbool(a->movhor);
+		a->movver=((a->y==b1->y-1)||(a->y==b2->y-1)) ? false : cbool(a->movver);
+		a->movver=((a->y==b1->y+1)||(a->y==b2->y+1)) ? true : a->movver;
+		b1->c=((b->y>b1->y+1)&&(b->y<b1->y-1)) ? b1->c+1: b1->c;
+		b2->c=((b->y>b2->y+1)&&(b->y<b2->y-1)) ? b2->c+1: b2->c;
+	}
+	a->x=(a->movhor==true) ? a->x+1 : a->x-1;
+	a->y=(a->movver==true) ? a->y+1 : a->y-1;
+}
+void main() {
+	objeto b1,b2,b[5],src; int control,cont=0; bool fin=false;
+	initgame(b1,b2,src);	
+	goto inicio;
 	while (fin != true) {
 		cont++;
 		usleep(4000);
-		if (cont%16==0) {
-		if ((b.y==s.y-1)||(b.y==1))
-			movver=cbool(movver);
-		if (((b.x==s.x-2)||(b.x==2))&&((b.y==b1.y)||(b.y==b1.y+1)||(b.y==b1.y-1)||(b.y==b2.y)||(b.y==b2.y-1)||(b.y==b2.y+1))){
-			movhor=cbool(movhor);
-			if ((b.y==b1.y-1)||(b.y==b2.y-1)) movver=false;
-			if ((b.y==b1.y+1)||(b.y==b2.y+1)) movver=true;
-		}
-		if ((b.x==s.x)||(b.x<=1)) { inicio: 
-			b1.c=(b.x<=1)?b1.c+1: b1.c; 
-			b2.c=(b.x>=s.x)?b2.c+1: b2.c;
-			b.y=s.y/2; b.x=s.x/2; 
-			movver=cbool(movver); movhor=cbool(movhor);
-		}
-		b.x=(movhor==true) ? b.x+1 : b.x-1;
-		b.y=(movver==true) ? b.y+1 : b.y-1;
-
+		if (cont%16==0){ if (cont%1000) {for (i=0;i<5;i++){engine(b[i],b1,b2,s);} }}
 		if (b1.y<=1) b1.y=s.y-2;
 		if (b1.y>=s.y-1) b1.y=2;
 		if (b2.y<=1) b2.y=s.y-2; 
 		if (b2.y>=s.y-1) b2.y=2;
-		}
-		fflush(stdin);			//limpia el buffer de la entrada estandar(teclado)
-		control=getch();		// Control
+		
+		fflush(stdin);
+		control=getch();
 		switch (control) {
 			
 			case KEY_DOWN:
