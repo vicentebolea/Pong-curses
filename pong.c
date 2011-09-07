bc701d6de63a2ce1e8b1792b258dc261345d1f8e
Vicente Adolfo Bolea Sánchez <vicente.bolea@gmail.com>
2011-09-07 20:09:34 +0900

    the original game the first version

--- pong.c
+++ pong.c
@@ -1,100 +1,128 @@
-#include <ncurses.h>		
+//Pong1.0 solo para linux
+//Hecho por Tito 
+//Utilizo la libreria ncurses y stdbool para las variables booleanas
+//los controles son los cursores de izq y drch y arriba para cerrar el juego
+//Para compilar "gcc pong.c -lncurses -o pong" en caso de que no tengais ncurses "sudo apt-get install ncurses"
+//Si quereis modificarlo hacerlo en una copia en vuestro pc
+#include <ncurses.h>
 #include <stdlib.h>
 #include <stdbool.h>
-typedef struct{short int x; short int y; short int c; bool movhor; bool movver;}objeto;
-bool cbool(bool a) {return a=(a==true) ? false : true;}
-void engine(objeto *a, objeto *b1, objeto *b2, objeto *scr) {
-	a->movver=((a->y==scr->y-1)||(a->y==1)) ? cbool(a->movver):a->movver;
-	if ((a->x >= scr->x-2)||(a->x <= 2)){
-		a->movhor=cbool(a->movhor);
-		a->movver=((a->y==b1->y-1)||(a->y==b2->y-1)) ? false : cbool(a->movver);
-		a->movver=((a->y==b1->y+1)||(a->y==b2->y+1)) ? true : a->movver;
-		if (((a->y > b1->y+1)||(a->y < b1->y-1))&&(a->x>=scr->x-2)) {b1->c++; a->x=scr->x/2; a->y=scr->y/2;}
-		if (((a->y > b2->y+1)||(a->y < b2->y-1))&&(a->x<=2)) {b2->c++; a->x=scr->x/2; a->y=scr->y/2;}
-	}
-	a->x=(a->movhor==true) ? a->x+1 : a->x-1;
-	a->y=(a->movver==true) ? a->y+1 : a->y-1;
-}
 void main() {
-	objeto b1,b2,b,s; int cont=0; bool fin=false;
+	int x = 20; int bx = 19; int control;
+	int y = 30; int by = 30; int cont = 0;
+	 
+	bool fin = false; bool primer = false; bool segun = false; bool tercer = false; bool cuarto = false;
 	
-	initscr();					
-	start_color();	
-	keypad(stdscr, true);
-	noecho();
-	curs_set(0);	
-	init_pair(1,COLOR_RED,COLOR_BLACK);
+	initscr(); 					//inciar la ventana
+	start_color(); 					//sin comentarios
+	keypad(stdscr, true); 				//activar teclas especiales como cursores...
+	noecho(); 					//Quitar el eco de las instrucciones
+	curs_set(0); 					//Ni papa
+	nodelay(stdscr,1); 				//Que no espere en el getch (no veas como me comi el coco con eso)
+
+	init_pair(1,COLOR_RED,COLOR_BLACK); 		//Establezco los colores a usar
 	init_pair(2,COLOR_BLUE,COLOR_BLACK);
-	init_pair(3,COLOR_WHITE,COLOR_BLACK);					
-	getmaxyx(stdscr,s.y,s.x);
-	b1.x=s.x-2; 	b2.x=1;		b.x=s.x/2;
-	b1.y=s.y/2; 	b2.y=s.y/2; 	b.y=s.y/2;
-	b1.c=0;		b2.c=0;
-	mvprintw(4,5,"          oooooooooo                                   "); 
-        mvprintw(5,5,"          888    888  ooooooo  oo oooooo     oooooooo8 ");
-        mvprintw(6,5,"          888oooo88 888     888 888   888  888    88o  ");
-        mvprintw(7,5,"          888       888     888 888   888   888oo888o  ");
-        mvprintw(8,5,"         o888o        88ooo88  o888o o888o 888     888 ");
-        mvprintw(9,5,"                                            888ooo888  ");
-        mvprintw(11,5,"Any questions please send me at vicente.bolea@gmail.com");
-	mvprintw(14,s.x/4,"Player 1 your controls are 'a' and 'q'");
-	mvprintw(15,s.x/4,"Player 2 your controls are the arrows of the keyboard");
-	mvprintw(17,s.x/4,"Push ANY key to start");
-	getch();
-			
-	nodelay(stdscr,1);
-	while (fin != true) {
+	init_pair(3,COLOR_WHITE,COLOR_BLACK);
+		tercer = true;				//Empieza el movimiento
+		segun = true;
+	while (fin != true) { 				// Comienzo
 		cont++;
-		usleep(4000);
-		if (cont%16==0) { engine(&b,&b1,&b2,&s);
-		if (b1.y<=1) b1.y=s.y-2;
-		if (b1.y>=s.y-1) b1.y=2;
-		if (b2.y<=1) b2.y=s.y-2; 
-		if (b2.y>=s.y-1) b2.y=2;
+		system("sleep 0.025");			//Tiempo de cada ciclo
+		if (cont%8==0) { 			//Esto es para regular la velocidad de juego
+		if (by==59) {				//Reglas de la partida, rebotes
+			cuarto = true; segun = false;
+			if (bx==1) {
+			tercer = false; primer = true;
+			}
 		}
-		switch (getch()) {
-			case KEY_DOWN:
-				b1.y++;
+		if (by==1) {
+			segun = true; cuarto = false;
+			if (bx==1) {
+			tercer = false; primer = true;
+			}
+		}
+		if (bx==1){
+			primer = true; tercer =false;
+		}
+		if ((bx == 20) && ((by == y) || (by==y+1) || (by==y-1))) { //Si toca con la bandeja 
+			primer = false; tercer = true;
+			}
+			else if (bx==20) {
+				
+				fin = true;
+			}
+		
+		if (primer == true){
+			bx++;
+		}
+		if (segun == true){
+			by++;
+		}		
+		if (tercer == true){
+			bx--;
+		}
+		if (cuarto == true){
+			by--;
+		}
+		if (y==1)
+			y=58;
+		if (y==59)
+			y=2;
+		}
+		control = getch();		// Control
+		switch (control) {
+			
+			case KEY_RIGHT:
+				y++;
+				break;
+			case KEY_LEFT:
+				y--;
 				break;
 			case KEY_UP:
-				b1.y--;
+				fin = true;
 				break;
-			case 113:
-				b2.y--;
-				break;
-			case 97:
-				b2.y++;
-				break;
-			case 112:
-				nodelay(stdscr,0);
-				mvprintw(s.y/2,s.x/4,"The game is paused, push ANY key to resume");
-				getch();
-				nodelay(stdscr,1);
-				break;
-			case 27:
-				fin=true;
+			default:
 				break;
 		}
-		erase();
-		box(stdscr,0,0);
-		attron(COLOR_PAIR(1));
-		mvprintw(b.y,b.x,"o");
-		attroff(COLOR_PAIR(1));	
-		attron(COLOR_PAIR(2));
-		mvprintw(s.y-4,s.x/2-4,"PINGPONG");
-		attroff(COLOR_PAIR(2));		
-		attron(COLOR_PAIR(3));
-		mvprintw(b1.y-1,b1.x,"|");
-		mvprintw(b1.y,b1.x,"|");
-		mvprintw(b1.y+1,b1.x,"|");
-		mvprintw(b2.y-1,b2.x,"|");
-		mvprintw(b2.y,b2.x,"|");
-		mvprintw(b2.y+1,b2.x,"|");
+		erase();			//Borra la pantalla para el refresco	
+		
+		attron(COLOR_PAIR(3));		//Movimiento de la bandeja
+		mvprintw(x,y-1,"___");
 		attroff(COLOR_PAIR(3));
-		mvprintw(2,s.x/11,"Player 1 : %i",b1.c);
-		mvprintw(2,4*s.x/5,"%i : Player 2",b2.c);
-		mvprintw(2,s.x/2-4,"time: %i",cont/8);
+		refresh();
+									
+		move(bx,by);			//Movimiento de la bola
+		attron(COLOR_PAIR(1));
+		printw("o");
+		attroff(COLOR_PAIR(1));
+		refresh();
+		
+		move(0,0);
+		printw("*");			//Cuadro de juego
+		hline(ACS_HLINE, 60);
+		move(1,0);		
+		vline(ACS_VLINE, 20);
+		move(1,60);
+		vline(ACS_VLINE, 20);
+		move(21,1);
+		hline(ACS_HLINE, 60);
+		mvprintw(0,60,"*");
+		mvprintw(21,0,"*");
+		mvprintw(21,60,"*");
+		
+		mvprintw(2,41,"bx:%i by:%i",bx,by); //Algunas letritas
+		mvprintw(3,41,"x:%i y:%i",x,y);
+		mvprintw(4,43,"tiempo: %i",cont);
+		attron(COLOR_PAIR(2));
+		mvprintw(5,41,"PINGPONG");
+		attroff(COLOR_PAIR(2));
+		refresh();
+		fflush(stdin);
 	}
 	erase();
+	refresh();
+        getch();
+	system("sleep 1");
         endwin();
 }
+
