a95793aa28b1162381917c17e88b785353edf981
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2012-01-07 13:31:54 +0100

    really good

--- pong.c
+++ pong.c
@@ -1,8 +1,7 @@
 #include <ncurses.h>
+typedef struct{short int x,y,c; bool movhor,movver;}object;
 
-typedef struct{short int x;  short int y; short int c; bool movhor; bool movver;}object;
-bool cbool(bool a) {return a ? false : true;}
-main(){	register object scr; register int i,cont=0; bool fin=false;	
+main(){	object scr; int i,cont=0; bool fin=false;	
 	initscr();					
 	start_color();
 	init_pair(1,COLOR_BLUE,COLOR_BLACK);	
@@ -21,38 +20,56 @@ main(){	register object scr; register int i,cont=0; bool fin=false;
 	mvprintw(14,scr.x/4,"Player 1 your controls are 'a' and 'q'");
 	mvprintw(15,scr.x/4,"Player 2 your controls are the arrows of the keyboard");
 	mvprintw(17,scr.x/4,"Push ANY key to start, 'p' for pause and ESC to quit");
-	getch();		
-	nodelay(stdscr,1);
-	while (!fin) {
-		usleep(4000);
-		if (++cont%16==0){if((b.y==scr.y-1)||(b.y==1)) b.movver=cbool(b.movver);
+	getch();
+	for (nodelay(stdscr,1); !fin; usleep(4000)) {
+		if (++cont%16==0){
+			if ((b.y==scr.y-1)||(b.y==1)) 
+				b.movver=!b.movver;
 			if ((b.x>=scr.x-2)||(b.x<=2)){
-				b.movhor=cbool(b.movhor);
-				if((b.y==b1.y-1)||(b.y==b2.y-1)) b.movver=false;
-				if((b.y==b1.y+1)||(b.y==b2.y+1)) b.movver=true;
-				if (((b.y > b1.y+1)||(b.y < b1.y-1))&&(b.x>=scr.x-2)) {b1.c++; b.x=scr.x/2; b.y=scr.y/2;}
-				if (((b.y > b2.y+1)||(b.y < b2.y-1))&&(b.x<=2)) {b2.c++; b.x=scr.x/2; b.y=scr.y/2;}}
-		b.x=b.movhor ? b.x+1 : b.x-1;
-		b.y=b.movver ? b.y+1 : b.y-1;
-		if (b1.y<=1) b1.y=scr.y-2;
-		if (b1.y>=scr.y-1) b1.y=2;
-		if (b2.y<=1) b2.y=scr.y-2; 
-		if (b2.y>=scr.y-1) b2.y=2;}
+				b.movhor=!b.movhor;
+				if ((b.y==b1.y)||(b.y==b2.y)){}
+				else if ((b.y==b1.y-1)||(b.y==b2.y-1)) 
+					b.movver=false;
+				else if ((b.y==b1.y+1)||(b.y==b2.y+1)) 
+					b.movver=true;
+				else if (b.x>=scr.x-2){
+					b1.c++; 
+					b.x=scr.x/2; 
+					b.y=scr.y/2;
+				}
+				else{
+					b2.c++; 
+					b.x=scr.x/2; 
+					b.y=scr.y/2;
+				}
+			}
+			b.x=b.movhor ? b.x+1 : b.x-1;
+			b.y=b.movver ? b.y+1 : b.y-1;
+			if (b1.y<=1) 
+				b1.y=scr.y-2;
+			if (b1.y>=scr.y-1) 
+				b1.y=2;
+			if (b2.y<=1) 
+				b2.y=scr.y-2; 
+			if (b2.y>=scr.y-1) 
+				b2.y=2;
+		}
 		switch (getch()) {
 			case KEY_DOWN:	b1.y++; break;
 			case KEY_UP: 	b1.y--; break;
 			case 'q':	b2.y--; break;
 			case 'a':	b2.y++; break;
-			case 'p':	nodelay(stdscr,0); getch(); nodelay(stdscr,1); break;
-			case 27: fin++; break;}
+			case 'p':	getchar(); break;
+			case 27: endwin(); fin++; break;
+		}
 		erase();
 		mvprintw(2,scr.x/2-2,"%i | %i",b1.c,b2.c);
-		move(0,scr.x/2);vline(ACS_VLINE,scr.y);
+		mvvline(0,scr.x/2,ACS_VLINE,scr.y);
 		attron(COLOR_PAIR(1));
 			mvprintw(b.y,b.x,"o");
 			for(i=-1;i<2;i++){
 				mvprintw(b1.y+i,b1.x,"|");
 				mvprintw(b2.y+i,b2.x,"|");}
 		attroff(COLOR_PAIR(1));
-	}endwin();
+	}
 }
