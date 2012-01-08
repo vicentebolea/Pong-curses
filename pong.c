a2b450fe7137289c81de2d0a9e97c0efb1d48574
vicentebolea <vicente.bolea@gmail.com>
2012-01-08 17:14:32 +0100

    fixed it

--- pong.c
+++ pong.c
@@ -1,31 +1,8 @@
 #include <ncurses.h>
 typedef struct{short int x,y,c; bool movhor,movver;}object;
 
-<<<<<<< HEAD
-typedef struct{short int x; short int y; short int c; bool movhor; bool movver;}objeto;
-bool cbool(bool a) {return a ? false : true;}
-void engine(objeto *a, objeto *b1, objeto *b2, objeto *scr) {
-	if((a->y==scr->y-1)||(a->y==1)) a->movver=cbool(a->movver);
-	if ((a->x>=scr->x-2)||(a->x<=2)){
-		a->movhor=cbool(a->movhor);
-		a->movver=((a->y==b1->y-1)||(a->y==b2->y-1)) ? false : cbool(a->movver);
-		a->movver=((a->y==b1->y+1)||(a->y==b2->y+1)) ? true : a->movver;
-		if (((a->y > b1->y+1)||(a->y < b1->y-1))&&(a->x>=scr->x-2)) {b1->c++; a->x=scr->x/2; a->y=scr->y/2;}
-		if (((a->y > b2->y+1)||(a->y < b2->y-1))&&(a->x<=2)) {b2->c++; a->x=scr->x/2; a->y=scr->y/2;}
-	}
-	a->x=a->movhor ? a->x+1 : a->x-1;
-	a->y=a->movver ? a->y+1 : a->y-1;
-	if (b1->y<=1) b1->y=scr->y-2;
-	if (b1->y>=scr->y-1) b1->y=2;
-	if (b2->y<=1) b2->y=scr->y-2; 
-	if (b2->y>=scr->y-1) b2->y=2;
-}
-main() {
-	objeto b1,b2,b,scr; int i,cont=0; bool fin=false;
-	
-=======
 main(){	object scr; int i,cont=0; bool fin=false;	
->>>>>>> minimalist
+	minimalist
 	initscr();					
 	start_color();
 	init_pair(1,COLOR_BLUE,COLOR_BLACK);	
@@ -45,13 +22,6 @@ main(){	object scr; int i,cont=0; bool fin=false;
 	mvprintw(15,scr.x/4,"Player 2 your controls are the arrows of the keyboard");
 	mvprintw(17,scr.x/4,"Push ANY key to start, 'p' for pause and ESC to quit");
 	getch();
-<<<<<<< HEAD
-			
-	nodelay(stdscr,1);
-	while (!fin) {
-		usleep(4000);
-		if (++cont%16==0) engine(&b,&b1,&b2,&scr);
-=======
 	for (nodelay(stdscr,1); !fin; usleep(4000)) {
 		if (++cont%16==0){
 			if ((b.y==scr.y-1)||(b.y==1)) 
@@ -85,7 +55,6 @@ main(){	object scr; int i,cont=0; bool fin=false;
 			if (b2.y>=scr.y-1) 
 				b2.y=2;
 		}
->>>>>>> minimalist
 		switch (getch()) {
 			case KEY_DOWN:	b1.y++; break;
 			case KEY_UP: 	b1.y--; break;
