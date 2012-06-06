3f1609a3eec29370ddd0adea7dc2055e984acdd1
Vicente Bolea <vicente.bolea@gmail.com>
2012-06-06 22:18:05 +0900

    inacabable

--- pong.c
+++ pong.c
@@ -3,32 +3,46 @@
 
 typedef struct{short int x,y,c; bool movhor,movver;}object;
 
+static object b,b1,b2;
+static bool fin;
+static int select,start=1;
+
 void *thread_function(void *arg)
 {
-	if (arg == 1)
-	{
-		switch (getch()) {
-			case KEY_DOWN:	b1.y++; break;
-			case KEY_UP: 	b1.y--; break;
-			case 'q':	b2.y--; break;
-			case 27: endwin(); fin++; break;
+	if (select == 1){
+		while (start!=0);
+		while (!fin){
+			fflush(stdin);
+			switch (getch()) {
+				case 'o':	b1.y++; break;
+				case 'l': 		b1.y--; break;
+				case 'q':				b2.y--; break;
+				case 27: 				endwin(); fin++; break;
+			}
 		}
-
-		
+		pthread_exit(0);
 	}
-	else if ( arg == 2)
-	{	
-		switch (getch()) {
-			case 'q':	b2.y--; break;
-			case 'a':	b2.y++; break;
-			case 'p':	getchar(); break;
-			case 27: endwin(); fin++; break;
+	else if (select == 2){
+		while (start!=0);
+		while (!fin){
+			fflush(stdin);
+			switch (getch()) {
+				case 'q':	b2.y--; break;
+				case 'a':	b2.y++; break;
+				case 'p':	getchar(); break;
+				case 27: 	endwin(); fin++; break;
+			}
 		}
-	}	
+		pthread_exit(0);
+	}
 }
 
 int main(){	
-	object scr; int i,cont=0; bool fin=false;
+	pthread_t a[2];
+	object scr; 
+	int i,cont=0; 
+
+
 	initscr();					
 	start_color();
 	init_pair(1,COLOR_BLUE,COLOR_BLACK);	
@@ -36,7 +50,22 @@ int main(){
 	noecho();
 	curs_set(0);						
 	getmaxyx(stdscr,scr.y,scr.x);
-	static object b1={scr.x-2,scr.y/2,0,false,false},b2={1,scr.y/2,0,false,false},b={scr.x/2,scr.y/2,0,false,false};
+
+	b1.x=scr.x-2;
+	b1.y=scr.y/2,0;
+	b1.c=0;
+	b1.movhor=b1.movver=false;
+
+	b2.x=1;
+	b2.y=scr.y/2,0;
+	b2.c=0;
+	b2.movhor=b2.movver=false;
+
+	b.x=scr.x/2;
+	b.y=scr.y/2;
+	b.c=0;
+	b.movhor=b.movver=false;
+
 	mvprintw(4,5,"          oooooooooo                                  "); 
 	mvprintw(5,5,"          888    888  ooooooo    ooooooo    oooooooo8");
 	mvprintw(6,5,"          888oooo88 888     888 888   888  888    88o ");
@@ -48,47 +77,55 @@ int main(){
 	mvprintw(15,scr.x/4,"Player 2 your controls are the arrows of the keyboard");
 	mvprintw(17,scr.x/4,"Push ANY key to start, 'p' for pause and ESC to quit");
 	getch();
-	for (nodelay(stdscr,1); !fin; usleep(4000)) {
-		if (++cont%16==0){
-			if ((b.y==scr.y-1)||(b.y==1)) 
-				b.movver=!b.movver;
-			if ((b.x>=scr.x-2)||(b.x<=2)){
-				b.movhor=!b.movhor;
-				if ((b.y==b1.y)||(b.y==b2.y)){}
-				else if ((b.y==b1.y-1)||(b.y==b2.y-1)) 
-					b.movver=false;
-				else if ((b.y==b1.y+1)||(b.y==b2.y+1)) 
-					b.movver=true;
-				else if (b.x>=scr.x-2){
-					b1.c++; 
-					b.x=scr.x/2; 
-					b.y=scr.y/2;
-				}
-				else{
-					b2.c++; 
-					b.x=scr.x/2; 
-					b.y=scr.y/2;
-				}
+	start=0;
+	select=1;
+	pthread_create(&a[0],NULL,thread_function,NULL);
+	select=2;
+	pthread_create(&a[1],NULL,thread_function,NULL);
+
+	for (nodelay(stdscr,1); !fin; usleep(40000)) {
+
+		if ((b.y==scr.y-1)||(b.y==1)) 
+			b.movver=!b.movver;
+		if ((b.x>=scr.x-2)||(b.x<=2)){
+			b.movhor=!b.movhor;
+			if ((b.y==b1.y)||(b.y==b2.y)){}
+			else if ((b.y==b1.y-1)||(b.y==b2.y-1)) 
+				b.movver=false;
+			else if ((b.y==b1.y+1)||(b.y==b2.y+1)) 
+				b.movver=true;
+			else if (b.x>=scr.x-2){
+				b1.c++; 
+				b.x=scr.x/2; 
+				b.y=scr.y/2;
 			}
-			b.x=b.movhor ? b.x+1 : b.x-1;
-			b.y=b.movver ? b.y+1 : b.y-1;
-			if (b1.y<=1) 
-				b1.y=scr.y-2;
-			if (b1.y>=scr.y-1) 
-				b1.y=2;
-			if (b2.y<=1) 
-				b2.y=scr.y-2; 
-			if (b2.y>=scr.y-1) 
-				b2.y=2;
+			else{
+				b2.c++; 
+				b.x=scr.x/2; 
+				b.y=scr.y/2;
+			}
 		}
-			erase();
+		b.x=b.movhor ? b.x+1 : b.x-1;
+		b.y=b.movver ? b.y+1 : b.y-1;
+		if (b1.y<=1) 
+			b1.y=scr.y-2;
+		if (b1.y>=scr.y-1) 
+			b1.y=2;
+		if (b2.y<=1) 
+			b2.y=scr.y-2; 
+		if (b2.y>=scr.y-1) 
+			b2.y=2;
+
+		erase();
 		mvprintw(2,scr.x/2-2,"%i | %i",b1.c,b2.c);
 		mvvline(0,scr.x/2,ACS_VLINE,scr.y);
 		attron(COLOR_PAIR(1));
-			mvprintw(b.y,b.x,"o");
-			for(i=-1;i<2;i++){
-				mvprintw(b1.y+i,b1.x,"|");
-				mvprintw(b2.y+i,b2.x,"|");}
-		attroff(COLOR_PAIR(1));
+		mvprintw(b.y,b.x,"o");
+		for(i=-1;i<2;i++){
+			mvprintw(b1.y+i,b1.x,"|");
+			mvprintw(b2.y+i,b2.x,"|");}
+			attroff(COLOR_PAIR(1));
 	}
+	pthread_join(a[0],NULL);
+	pthread_join(a[1],NULL);
 }
